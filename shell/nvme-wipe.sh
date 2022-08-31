b6a617ea813fd7eb3e90f58ed071bb11b88d40dc
carl.liebich <carl.liebich@mujin.co.jp>
2022-08-31 14:28:19 +0900

    updates to multi disk handling

--- shell/nvme-wipe.sh
+++ shell/nvme-wipe.sh
@@ -19,39 +19,52 @@
 sudo apt update
 sudo apt install nvme-cli -y
 clear
-echo ""
+echo "🔹 Updating firmware..."
+sudo fwupdmgr update -y --no-reboot-check
+echo " "
+
+echo "------------------------------"
+echo "Note:"
+echo "If you see this below error, suspend and wake up the PC:"
+echo "Example Error - NVMe status: INVALID_FORMAT: The LBA Format specified is not supported. This may be due to various conditions(0x410a)"
+echo "------------------------------"
+
+echo " "
 
 for DISK in "$@"
 do
 
-    echo ""
+    echo " "
     echo "☢ This process will destory all data on:"
     echo "------------------------------"
-    sudo nvme list /dev/$DISK
+    sudo nvme list | grep $DISK
     echo "------------------------------"
-    echo ""
+    echo " "
     echo "🔹 Now erasing $DISK..."
     sudo nvme format -s1 /dev/$DISK --force
-    echo "------------------------------"
-    echo ""
-
+    sudo nvme format /dev/$DISK --force
+    echo " "
     echo "🔹 Creating msdos partition label..."
     sudo parted -s /dev/$DISK mklabel msdos
 
-    echo "🔹 Updating firmware..."
-    sudo fwupdmgr update -y --no-reboot-check
-    echo ""
-
     # REPORT GENERATION:
-    DISK=$(lsblk /dev/$DISK -o model,serial | tail -n +2)
-    DISK_FILE=${DISK// /_}
-    touch $DISK_FILE.txt
-    sudo nvme list /dev/$DISK > $DISK_FILE.txt
-    echo " " >> $DISK_FILE.txt
-    echo "##################################" >> $DISK_FILE.txt
-    echo "SSD DISK HEALTH REPORT" >> $DISK_FILE.txt
-    echo "##################################" >> $DISK_FILE.txt
-    sudo nvme smart-log /dev/$DISK >> $DISK_FILE.txt
+    echo "🔹 Genearting disk report..."
+    DISK_MS=$(lsblk /dev/$DISK -o model,serial | tail -n +2)
+    DISK_FILE=${DISK_MS// /_}.txt
+    touch $DISK_FILE
+    echo "NVME Disk Report:" > $DISK_FILE
+    echo " " >> $DISK_FILE
+    date >> $DISK_FILE
+    echo " " >> $DISK_FILE
+    echo $DISK_MS >> $DISK_FILE 
+    echo " " >> $DISK_FILE
+    sudo nvme list | grep Node >> $DISK_FILE
+    sudo nvme list | grep $DISK >> $DISK_FILE
+    echo " " >> $DISK_FILE
+    echo "##################################" >> $DISK_FILE
+    echo "SSD DISK HEALTH REPORT" >> $DISK_FILE
+    echo "##################################" >> $DISK_FILE
+    sudo nvme smart-log /dev/$DISK >> $DISK_FILE
     echo ""
     echo "🏁 $DISK Done"
 
